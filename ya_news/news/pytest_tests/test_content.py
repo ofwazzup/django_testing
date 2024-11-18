58a75a31420a21536056ca8d7e81fd839dc149a2
Ruslan Sibogatov <106180703+ofwazzup@users.noreply.github.com>
2024-11-18 04:24:35 +0300

    Update test_content.py

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -4,10 +4,7 @@ from django.conf import settings
 
 @pytest.mark.django_db
 def test_news_count(eleven_news, url_news_home, client):
-    """
-    Проверка, что на главной странице выводится не более 10 новостей.
-    """
-
+    """Проверка, что на главной странице выводится не более 10 новостей."""
     response = client.get(url_news_home)
     object_list = response.context['object_list']
     news_count = len(object_list)
@@ -16,10 +13,7 @@ def test_news_count(eleven_news, url_news_home, client):
 
 @pytest.mark.django_db
 def test_news_order(eleven_news, url_news_home, client):
-    """
-    Проверка, сортировки от самой свежей к самой старой.
-    """
-
+    """Проверка, сортировки от самой свежей к самой старой."""
     response = client.get(url_news_home)
     object_list = response.context['object_list']
     all_dates = [news.date for news in object_list]
@@ -29,10 +23,7 @@ def test_news_order(eleven_news, url_news_home, client):
 
 @pytest.mark.django_db
 def test_comments_order(news_with_ten_comments, url_news_detail, client):
-    """
-    Старые комментарии должны быть в начале, новые — в конце.
-    """
-
+    """Старые комментарии должны быть в начале, новые — в конце."""
     response = client.get(url_news_detail)
     assert 'news' in response.context
     news = response.context['news']
@@ -45,20 +36,14 @@ def test_comments_order(news_with_ten_comments, url_news_detail, client):
 
 @pytest.mark.django_db
 def test_anonymous_client_has_no_form(url_news_detail, client):
-    """
-    Проверка, что анонимному пользователю недоступна форма комментария.
-    """
-
+    """Проверка, что анонимному пользователю недоступна форма комментария."""
     response = client.get(url_news_detail)
     assert 'form' not in response.context
 
 
 @pytest.mark.django_db
 def test_authorized_client_has_form(url_news_detail, author_client):
-    """
-    Проверка, что авторизованный пользователь видит форму комментария.
-    """
-
+    """Проверка, что авторизованный пользователь видит форму комментария."""
     response = author_client.get(url_news_detail)
     assert 'form' in response.context
     assert type(response.context['form']).__name__ == 'CommentForm'
