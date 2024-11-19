d46f128f4dc909f5e900a2e7198162a8daed49e1
Ruslan Sibogatov <106180703+ofwazzup@users.noreply.github.com>
2024-11-19 09:55:12 +0300

    Update test_content.py

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -1,8 +1,9 @@
 import pytest
 from django.conf import settings
 
+pytestmark = pytest.mark.django_db
 
-@pytest.mark.django_db
+
 def test_news_count(eleven_news, url_news_home, client):
     """Проверка, что на главной странице выводится не более 10 новостей."""
     response = client.get(url_news_home)
@@ -11,7 +12,6 @@ def test_news_count(eleven_news, url_news_home, client):
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
-@pytest.mark.django_db
 def test_news_order(eleven_news, url_news_home, client):
     """Проверка, сортировки от самой свежей к самой старой."""
     response = client.get(url_news_home)
@@ -21,7 +21,6 @@ def test_news_order(eleven_news, url_news_home, client):
     assert all_dates == sorted_dates
 
 
-@pytest.mark.django_db
 def test_comments_order(news_with_ten_comments, url_news_detail, client):
     """Старые комментарии должны быть в начале, новые — в конце."""
     response = client.get(url_news_detail)
@@ -29,21 +28,19 @@ def test_comments_order(news_with_ten_comments, url_news_detail, client):
     news = response.context['news']
     all_comments = news.comment_set.all()
 
-    for i in range(all_comments.count() - 1):
-        if all_comments[i].created > all_comments[i + 1].created:
-            assert False, 'Комментарии отсортированы неправильно'
+    created_list = [comment.created for comment in all_comments]
+    assert created_list == sorted(created_list), 'Комментарии отсортированы неправильно'
 
 
-@pytest.mark.django_db
 def test_anonymous_client_has_no_form(url_news_detail, client):
     """Проверка, что анонимному пользователю недоступна форма комментария."""
     response = client.get(url_news_detail)
     assert 'form' not in response.context
 
 
-@pytest.mark.django_db
 def test_authorized_client_has_form(url_news_detail, author_client):
     """Проверка, что авторизованный пользователь видит форму комментария."""
     response = author_client.get(url_news_detail)
     assert 'form' in response.context
-    assert type(response.context['form']).__name__ == 'CommentForm'
+    from news.forms import CommentForm
+    assert isinstance(response.context['form'], CommentForm)
