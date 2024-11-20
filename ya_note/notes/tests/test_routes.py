ab2da2ce17d6ff6026524d64964573b7ad409ede
Ruslan Sibogatov <106180703+ofwazzup@users.noreply.github.com>
2024-11-20 13:32:51 +0300

    Update test_routes.py

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -3,9 +3,11 @@ from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
-
 from notes.models import Note
 
+from test_utils import setUpTestData
+
+# Константы для URL
 URL_HOME = reverse('notes:home')
 URL_LOGIN = reverse('users:login')
 URL_LOGOUT = reverse('users:logout')
@@ -13,6 +15,9 @@ URL_SIGNUP = reverse('users:signup')
 URL_NOTES_LIST = reverse('notes:list')
 URL_NOTES_ADD = reverse('notes:add')
 URL_NOTES_SUCCESS = reverse('notes:success')
+URL_NOTE_DETAIL = reverse('notes:detail', args=('sample-slug',))
+URL_NOTE_EDIT = reverse('notes:edit', args=('sample-slug',))
+URL_NOTE_DELETE = reverse('notes:delete', args=('sample-slug',))
 
 
 class RoutesTests(TestCase):
@@ -20,50 +25,30 @@ class RoutesTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.author_user = get_user_model().objects.create(username='Author')
-        cls.reader_user = get_user_model().objects.create(username='Reader')
-        cls.author_client = Client()
-        cls.reader_client = Client()
-        cls.author_client.force_login(cls.author_user)
-        cls.reader_client.force_login(cls.reader_user)
-
-        cls.note = Note.objects.create(
-            title='Sample Title',
-            text='Sample Text',
-            slug='sample-slug',
-            author=cls.author_user,
-        )
-
-        cls.url_note_detail = reverse('notes:detail', args=(cls.note.slug,))
-        cls.url_note_edit = reverse('notes:edit', args=(cls.note.slug,))
-        cls.url_note_delete = reverse('notes:delete', args=(cls.note.slug,))
+        # Используем setUpTestData из test_utils
+        setUpTestData(cls)
 
     def test_public_and_authenticated_accessibility(self):
-        """
-        Проверка кодов возврата
-        для публичных и аутентифицированных пользователей.
-        """
+        """Проверка кодов возврата для публичных и аутентифицированных пользователей."""
+
         public_urls = [URL_HOME, URL_LOGIN, URL_LOGOUT, URL_SIGNUP]
         authenticated_urls = [URL_NOTES_LIST, URL_NOTES_ADD, URL_NOTES_SUCCESS]
 
-        # Проверка публичных страниц
-        for url in public_urls:
+        # Проверка публичных страниц и страниц для аутентифицированных пользователей
+        for url in public_urls + authenticated_urls:
             with self.subTest(url=url):
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
-
-        # Проверка страниц для аутентифицированного пользователя
-        for url in authenticated_urls:
-            with self.subTest(url=url):
-                response = self.reader_client.get(url)
+                # Выбор клиента для аутентифицированных пользователей
+                client = self.client if url in public_urls else self.reader_client
+                response = client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_protected_pages_redirect_and_authorization(self):
         """Проверка редиректов и доступа к защищенным страницам."""
+
         protected_urls = [
-            self.url_note_detail,
-            self.url_note_edit,
-            self.url_note_delete,
+            URL_NOTE_DETAIL,
+            URL_NOTE_EDIT,
+            URL_NOTE_DELETE,
             URL_NOTES_ADD,
             URL_NOTES_SUCCESS,
             URL_NOTES_LIST,
@@ -75,18 +60,3 @@ class RoutesTests(TestCase):
                 redirect_url = f'{URL_LOGIN}?next={url}'
                 response = self.client.get(url)
                 self.assertRedirects(response, redirect_url)
-
-        # Проверка доступа для авторизованных пользователей
-        restricted_urls = [
-            self.url_note_detail, self.url_note_edit, self.url_note_delete
-        ]
-        user_status_pairs = [
-            (self.author_client, HTTPStatus.OK),
-            (self.reader_client, HTTPStatus.NOT_FOUND),
-        ]
-
-        for client, expected_status in user_status_pairs:
-            for url in restricted_urls:
-                with self.subTest(client=client, url=url):
-                    response = client.get(url)
-                    self.assertEqual(response.status_code, expected_status)
