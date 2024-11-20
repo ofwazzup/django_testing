a65d81dcbf2daec2ad16da2fdb50716e1286ab9c
Ruslan Sibogatov <106180703+ofwazzup@users.noreply.github.com>
2024-11-20 12:51:53 +0300

    Update test_routes.py

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -7,54 +7,43 @@ pytestmark = pytest.mark.django_db
 
 
 @pytest.mark.parametrize(
-    'url',
+    'url, client, expected_status',
     (
-        pytest.lazy_fixture('url_news_home'),  # Главная страница
-        pytest.lazy_fixture('url_user_login'),  # Страница входа
-        pytest.lazy_fixture('url_user_logout'),  # Страница выхода
-        pytest.lazy_fixture('url_user_signup'),  # Страница регистрации
+        (pytest.lazy_fixture('url_news_home'), 'client', HTTPStatus.OK),
+        (pytest.lazy_fixture('url_user_login'), 'client', HTTPStatus.OK),
+        (pytest.lazy_fixture('url_user_logout'), 'client', HTTPStatus.OK),
+        (pytest.lazy_fixture('url_user_signup'), 'client', HTTPStatus.OK),
+        (pytest.lazy_fixture('url_news_detail'), 'client', HTTPStatus.OK),
+        (pytest.lazy_fixture(
+            'url_comment_edit'), 'author_client', HTTPStatus.OK
+        ),
+        (pytest.lazy_fixture(
+            'url_comment_delete'), 'author_client', HTTPStatus.OK
+        ),
+        (pytest.lazy_fixture(
+            'url_comment_edit'), 'admin_client', HTTPStatus.NOT_FOUND
+        ),
+        (pytest.lazy_fixture(
+            'url_comment_delete'), 'admin_client', HTTPStatus.NOT_FOUND
+        ),
     )
 )
-def test_pages_availability_for_anonymous_user(client, url):
-    """Проверяет доступность страниц для анонима."""
+def test_page_status_codes(url, client, expected_status):
+    """Проверяет код ответа для различных страниц и пользователей."""
     response = client.get(url)
-    assert response.status_code == HTTPStatus.OK
-
-
-def test_detail_page_availability(url_news_detail, client):
-    """Проверяет доступность страницы новости для анонима."""
-    response = client.get(url_news_detail)
-    assert response.status_code == HTTPStatus.OK
-
-
-@pytest.mark.parametrize(
-    'parametrized_client, expected_status',
-    (
-        (pytest.lazy_fixture('author_client'), HTTPStatus.OK),
-        (pytest.lazy_fixture('admin_client'), HTTPStatus.NOT_FOUND),
-    ),
-)
-@pytest.mark.parametrize(
-    'url',
-    (
-        pytest.lazy_fixture('url_comment_edit'),
-        pytest.lazy_fixture('url_comment_delete'),
-    ),
-)
-def test_pages_availability_for_different_users(
-    parametrized_client, url, expected_status
-):
-    """Проверяет доступность страниц для разных пользователей."""
-    response = parametrized_client.get(url)
     assert response.status_code == expected_status
 
 
 @pytest.mark.parametrize(
-    'url',
+    'url, url_user_login',
     (
-        pytest.lazy_fixture('url_comment_edit'),
-        pytest.lazy_fixture('url_comment_delete'),
-    ),
+        (pytest.lazy_fixture(
+            'url_comment_edit'
+        ), pytest.lazy_fixture('url_user_login')),
+        (pytest.lazy_fixture(
+            'url_comment_delete'
+        ), pytest.lazy_fixture('url_user_login')),
+    )
 )
 def test_redirects_for_anonymous_user(url, url_user_login, client):
     """Проверяет редирект для анонимного пользователя."""
