0d3e12564469e43cfb49c9ded76c8b5b311ae553
Ruslan Sibogatov <106180703+ofwazzup@users.noreply.github.com>
2024-11-20 13:24:13 +0300

    Update test_logic.py

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,34 +1,31 @@
 from http import HTTPStatus
 from django.test import Client, TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
 from pytils.translit import slugify
 from notes.forms import WARNING
 from notes.models import Note
+from test_utils import (
+    BaseNoteTestCase,
+    URL_ADD_NOTE,
+    URL_LOGIN,
+    URL_SUCCESS_PAGE,
+    SLUG,
+    get_edit_url
+)
 
-URL_ADD_NOTE = reverse('notes:add')
-URL_LOGIN = reverse('users:login')
-URL_SUCCESS_PAGE = reverse('notes:success')
-SLUG = 'note-slug'
 
-
-class NoteManagementTestCase(TestCase):
+class NoteManagementTestCase(BaseNoteTestCase, TestCase):
     """Тесты для проверки операций с заметками."""
 
     @classmethod
     def setUpTestData(cls):
         """Инициализация тестовых данных."""
-        cls.author_user = User.objects.create(username='Автор')
-        cls.reader_user = User.objects.create(username='Читатель')
-        cls.author_client = Client()
-        cls.reader_client = Client()
-        cls.author_client.force_login(cls.author_user)
-        cls.reader_client.force_login(cls.reader_user)
+        super().setUpTestData()
         cls.existing_note = Note.objects.create(
             title='Заголовок',
             text='Текст заметки',
-            slug=SLUG,  # Использование константы
-            author=cls.author_user,
+            slug=SLUG,
+            author=cls.author,
         )
         cls.new_note_data = {
             'title': 'Новый заголовок',
@@ -36,13 +33,9 @@ class NoteManagementTestCase(TestCase):
             'slug': 'new-slug',
         }
         cls.initial_note_count = Note.objects.count()
-        cls.url_edit_note = reverse(
-            'notes:edit',
-            args=(cls.existing_note.slug,)
-        )
+        cls.url_edit_note = get_edit_url(cls.existing_note.slug)
         cls.url_delete_note = reverse(
-            'notes:delete',
-            args=(cls.existing_note.slug,)
+            'notes:delete', args=(cls.existing_note.slug,)
         )
 
     def test_create_note_authenticated_user(self):
@@ -56,7 +49,7 @@ class NoteManagementTestCase(TestCase):
         self.assertEqual(created_note.title, self.new_note_data['title'])
         self.assertEqual(created_note.text, self.new_note_data['text'])
         self.assertEqual(created_note.slug, self.new_note_data['slug'])
-        self.assertEqual(created_note.author, self.author_user)
+        self.assertEqual(created_note.author, self.author)
 
     def test_create_note_anonymous_user(self):
         """Анонимный пользователь не может создавать заметки."""
@@ -67,7 +60,7 @@ class NoteManagementTestCase(TestCase):
 
     def test_create_note_duplicate_slug(self):
         """Нельзя создать заметку с дублирующимся slug."""
-        self.new_note_data['slug'] = SLUG  # Использование константы
+        self.new_note_data['slug'] = SLUG
         response = self.author_client.post(
             URL_ADD_NOTE, data=self.new_note_data
         )
