f36aad03fcbe0cb6df1883fdd93d5a5723876be4
Ruslan Sibogatov <106180703+ofwazzup@users.noreply.github.com>
2024-11-21 11:52:12 +0300

    Update test_utils.py

--- ya_note/notes/tests/test_utils.py
+++ ya_note/notes/tests/test_utils.py
@@ -3,18 +3,14 @@ from django.test import Client
 from notes.models import Note
 from django.urls import reverse
 
+# Константы для URL
 URL_ADD_NOTE = reverse('notes:add')
 URL_LOGIN = reverse('users:login')
 URL_SUCCESS_PAGE = reverse('notes:success')
-SLUG = 'note-slug'
 URL_NOTES_LIST = reverse('notes:list')
 URL_HOME = reverse('notes:home')
-URL_LOGIN = reverse('users:login')
 URL_LOGOUT = reverse('users:logout')
 URL_SIGNUP = reverse('users:signup')
-URL_NOTES_LIST = reverse('notes:list')
-URL_NOTES_ADD = reverse('notes:add')
-URL_NOTES_SUCCESS = reverse('notes:success')
 URL_NOTE_DETAIL = reverse(
     'notes:detail', args=('sample-slug',)
 )
@@ -34,19 +30,15 @@ def get_edit_url(slug):
     return reverse('notes:edit', args=(slug,))
 
 
-# Базовый класс для тестирования заметок
+# Базовый класс для тестирования
 class BaseNoteTestCase:
     """Базовый класс для тестирования заметок."""
 
     @classmethod
     def setUpTestData(cls):
         """Инициализация тестовых данных."""
-        cls.author = User.objects.create(
-            username='Автор'
-        )
-        cls.reader = User.objects.create(
-            username='Читатель'
-        )
+        cls.author = User.objects.create(username='Автор')
+        cls.reader = User.objects.create(username='Читатель')
         cls.author_client = Client()
         cls.reader_client = Client()
         cls.author_client.force_login(cls.author)
@@ -54,6 +46,6 @@ class BaseNoteTestCase:
         cls.note = Note.objects.create(
             title='Заголовок',
             text='Текст заметки',
-            slug=SLUG,
+            slug='note-slug',
             author=cls.author,
         )
